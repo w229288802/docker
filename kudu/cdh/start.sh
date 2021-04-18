c34917b9992a6239cc93eaaf39b84cbae47924d0
btu <229288802@qq.com>
2021-04-18 18:49:17 +0800

    classify

--- kudu/cdh/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget https://archive.cloudera.com/cdh5/redhat/6/x86_64/cdh/cloudera-cdh5.repo
-mv cloudera-cdh5.repo /etc/yum.repos.d/
-yum install -y yum-utils createrepo
-reposync -r cloudera-cdh5
