3c04bf2892c9058af119e1db65e7f1eba1d2b52b
btu <229288802@qq.com>
2021-04-20 00:09:13 +0800

    test

--- hadoop/hadoop-2.7.5/build-support/docker-entrypoint.sh
+++ hadoop/hadoop-2.7.5/build-support/docker-entrypoint.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+export JAVA_HOME=/usr
 if [[ ! -f ~/.ssh/id_rsa ]];then
-ssh-keygen -t rsa -f /root/.ssh/id_rsa -P ''
+echo
+
+
+    #/opt/hadoop-2.7.5/bin/hadoop namenode -format
 fi
-if [[ $HOSTNAME == "" ]];then
-    echo "test"
+ssh-keygen -A -t rsa -P ''
+/usr/sbin/sshd
+ssh master ls /
+ssh slave1 ls /
+if [[ $HOSTNAME == "master" ]];then
+ls ~/.ssh/
+ssh master ls /
+ssh slave1 ls /
+ echo
+    #/opt/hadoop-2.7.5/sbin/start-all.sh
 fi
-echo "started"
+#ping master
+echo "==========================>>> started <<<=========================="
--- hadoop/hadoop-2.7.5/start-after-build.sh
+++ hadoop/hadoop-2.7.5/start-after-build.sh
@@ -4,21 +4,26 @@ DOCKER_VOLUME_DIR=/var/lib/docker/volumes
 FILE_DIR=$(cd $(dirname $0); pwd)
 
 if [[ ! -f ${FILE_DIR}/hadoop-2.7.5.tar.gz ]];then
-echo "download hadoop to ${FILE_DIR}..."
+echo "==========================>>> download hadoop to ${FILE_DIR} <<<=========================="
 wget http://archive.apache.org/dist/hadoop/core/hadoop-2.7.5/hadoop-2.7.5.tar.gz -P ${FILE_DIR}/
 fi
 
 if [[ ! -d ${FILE_DIR}/hadoop-2.7.5 ]];then
+echo "==========================>>> express hadoop to ${FILE_DIR} <<<=========================="
 tar -xvf hadoop-2.7.5.tar.gz -C ${FILE_DIR}/
 fi
 
 
-if [[ ! -d ${DOCKER_VOLUME_DIR}/hadoop-bin ]]; then
-docker volume create hadoop-bin
-cp ./hadoop-2.7.5 /var/lib/docker/volumes/hadoop-bin/_data/
+if [[ ! -d ${DOCKER_VOLUME_DIR}/hadoop-opt ]]; then
+echo "==========================>>> create volume to ${FILE_DIR} <<<=========================="
+docker volume create hadoop-opt
 fi
 
+cp  -R ${FILE_DIR}/hadoop-2.7.5 ${DOCKER_VOLUME_DIR}/hadoop-opt/_data/
+cp  -R ${FILE_DIR}/etc ${DOCKER_VOLUME_DIR}/hadoop-opt/_data/hadoop-2.7.5/
+
+echo "==========================>>> build image to ${FILE_DIR} <<<=========================="
 docker build --rm -t hadoop:v2.7.5 ${FILE_DIR}/build-support
-echo "===>>> starting <<<==="
-docker-compose up
-#docker run --rm --name hadoop -it -v $(cd $(dirname $0); pwd)/hadoop-2.7.5:/opt/hadoop hadoop:v2.7.5 bash
+echo "==========================>>> starting <<<=========================="
+docker-compose up --remove-orphans
+#docker run --rm --name hadoop -it -v ${FILE_DIR}:/opt/hadoop hadoop:v2.7.5 bash
