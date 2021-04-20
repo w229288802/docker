eb505348a192cd4f9680c59b8dda79ace04705b9
wuwj <229288802@qq.com>
2021-04-20 17:14:18 +0800

    hadoop can start

--- hadoop/hadoop-2.7.5/build-support/docker-entrypoint.sh
+++ hadoop/hadoop-2.7.5/build-support/docker-entrypoint.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
-export JAVA_HOME=/usr
-if [[ ! -f ~/.ssh/id_rsa ]];then
-echo
+S='\033[0;30m[debug] '
+E='\033[0m'
 
-
-    #/opt/hadoop-2.7.5/bin/hadoop namenode -format
-fi
-ssh-keygen -A -t rsa -P ''
 /usr/sbin/sshd
-ssh master ls /
-ssh slave1 ls /
-if [[ $HOSTNAME == "master" ]];then
-ls ~/.ssh/
-ssh master ls /
-ssh slave1 ls /
- echo
-    #/opt/hadoop-2.7.5/sbin/start-all.sh
+
+mkdir -p /root/.ssh/
+cp /etc/ssh/ssh_host_rsa_key ~/.ssh/id_rsa
+cp /etc/ssh/ssh_host_rsa_key.pub ~/.ssh/authorized_keys
+echo -n "master " > ~/.ssh/known_hosts &&  cat /etc/ssh/ssh_host_rsa_key.pub >>  ~/.ssh/known_hosts
+echo -n "slave1 " >> ~/.ssh/known_hosts &&  cat /etc/ssh/ssh_host_rsa_key.pub >> ~/.ssh/known_hosts
+
+# 开启hadoop
+if [[ "${HOSTNAME}" == "master" ]]; then
+ls /opt
+/opt/hadoop-2.7.5/sbin/start-all.sh
+else
+
+# 等待master启
+while nc -w 1 master 22 </dev/null >/dev/null 2>&1;[[ $? == 1 || $? == 2 ]]
+do
+ sleep 0.2s
+done
+echo -e "${S}check activated master${E}"
+# 等待 master退出,注意！！！不然会找不到slave，也可以用sleep等待
+while nc -w 1 master 22 </dev/null >/dev/null 2>&1;[[ $? == 0 || $? == 2 ]]
+do
+  sleep 0.2s
+done
+echo -e "${S}check inactivated master${E}"
+
 fi
-#ping master
-echo "==========================>>> started <<<=========================="
--- hadoop/hadoop-2.7.5/start-after-build.sh
+++ hadoop/hadoop-2.7.5/start-after-build.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
+S='\033[0;30m[debug] '
+E='\033[0m'
 
 DOCKER_VOLUME_DIR=/var/lib/docker/volumes
 FILE_DIR=$(cd $(dirname $0); pwd)
 
 if [[ ! -f ${FILE_DIR}/hadoop-2.7.5.tar.gz ]];then
-echo "==========================>>> download hadoop to ${FILE_DIR} <<<=========================="
+echo -e "${S}downloading hadoop to ${FILE_DIR}${E}"
 wget http://archive.apache.org/dist/hadoop/core/hadoop-2.7.5/hadoop-2.7.5.tar.gz -P ${FILE_DIR}/
 fi
 
 if [[ ! -d ${FILE_DIR}/hadoop-2.7.5 ]];then
-echo "==========================>>> express hadoop to ${FILE_DIR} <<<=========================="
+echo -e "${S}expressing hadoop to ${FILE_DIR}${E}"
 tar -xvf hadoop-2.7.5.tar.gz -C ${FILE_DIR}/
 fi
 
 
 if [[ ! -d ${DOCKER_VOLUME_DIR}/hadoop-opt ]]; then
-echo "==========================>>> create volume to ${FILE_DIR} <<<=========================="
+echo -e "${S}creating volume to ${FILE_DIR}${E}"
 docker volume create hadoop-opt
-fi
-
 cp  -R ${FILE_DIR}/hadoop-2.7.5 ${DOCKER_VOLUME_DIR}/hadoop-opt/_data/
 cp  -R ${FILE_DIR}/etc ${DOCKER_VOLUME_DIR}/hadoop-opt/_data/hadoop-2.7.5/
+fi
 
-echo "==========================>>> build image to ${FILE_DIR} <<<=========================="
+
+
+echo -e "${S}building image to ${FILE_DIR}${E}"
 docker build --rm -t hadoop:v2.7.5 ${FILE_DIR}/build-support
-echo "==========================>>> starting <<<=========================="
+echo -e "${S}starting hadoop docker-compose${E}"
 docker-compose up --remove-orphans
 #docker run --rm --name hadoop -it -v ${FILE_DIR}:/opt/hadoop hadoop:v2.7.5 bash
