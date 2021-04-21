c4b73dbeba4e079f5b636a5c24b5e2ef48f6cfe5
wuwj <229288802@qq.com>
2021-04-21 18:20:50 +0800

    kill old process

---
+++ hadoop/spark-2.2.0-standalone/README.md
@@ -0,0 +1,48 @@
+#1.下载 Spark 安装包, 下载时候选择对应的 Hadoop 版本(资料中已经提供了 Spark 安装包, 直接上传至集群 Master 即可, 无需遵循以下步骤)
+cd /export/softwares
+wget https://archive.apache.org/dist/spark/spark-2.2.0/spark-2.2.0-bin-hadoop2.7.tgz
+
+#2.解压并拷贝到`export/servers`
+tar xzvf spark-2.2.0-bin-hadoop2.7.tgz
+mv spark-2.2.0-bin-hadoop2.7.tgz /export/servers/spark
+
+#3.修改配置文件`spark-env.sh`, 以指定运行参数
+cd /export/servers/spark/conf
+cp spark-env.sh.template spark-env.sh
+vi spark-env.sh
+
+#4.添加以下内容到spark-env.sh末尾
+export JAVA_HOME=/export/servers/jdk1.8.0
+export SPARK_MASTER_HOST=node01
+export SPARK_MASTER_PORT=7077
+
+#5.修改配置文件 slaves,
+cd /export/servers/spark/conf
+cp slaves.template slaves
+vi slaves
+
+#6.配置 HistoryServer
+cd /export/servers/spark/conf
+cp spark-defaults.conf.template spark-defaults.conf
+vi spark-defaults.conf
+#4.添加以下内容到spark-defaults.conf末尾
+spark.eventLog.enabled  true
+spark.eventLog.dir      hdfs://node01:8020/spark_log
+spark.eventLog.compress true
+
+#8.将以下内容复制到`spark-env.sh`的末尾, 配置 HistoryServer 启动参数, 使得 HistoryServer 在启动的时候读取 HDFS 中写入的 Spark 日志
+export SPARK_HISTORY_OPTS="-Dspark.history.ui.port=4000 -Dspark.history.retainedApplications=3 -Dspark.history.fs.logDirectory=hdfs://node01:8020/spark_log"
+
+
+为 Spark 创建 HDFS 中的日志目录
+hdfs dfs -mkdir -p /spark_log
+
+将 Spark 安装包分发给集群中其它机器
+cd /export/servers
+scp -r spark root@node02:$PWD
+scp -r spark root@node03:$PWD
+
+启动 Spark Master 和 Slaves, 以及 HistoryServer
+cd /export/servers/spark
+sbin/start-all.sh
+sbin/start-history-server.sh
---
+++ hadoop/spark-2.2.0-standalone/start-after-build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+BLACK_COLOR='\033[0;30m'
+NO_COLOR='\033[0m'
+printf_and_run() {
+printf "${BLACK_COLOR}$1 ${NO_COLOR}\n"
+bash -c "$1"
+}
+
+#配置变量
+JAVA_HOME=/usr
+DOCKER_VOLUME_DIR=/var/lib/docker/volumes
+HADOOP_VERSION=hadoop2.7
+SPARK_HOME_VOLUME=hadoop_opt
+SPARK_VERSION=2.2.0
+SPARK_DIR=spark-${SPARK_VERSION}
+SPARK_MASTER_HOST=spark-master
+SPARK_MASTER_PORT=7077
+FILE_DIR=$(cd $(dirname $0); pwd)
+
+
+# 下载spark包
+if [[ ! -f ${FILE_DIR}/${SPARK_DIR}-bin-${HADOOP_VERSION}.tgz ]];then
+printf_and_run "wget https://archive.apache.org/dist/spark/${SPARK_DIR}/${SPARK_DIR}-bin-${HADOOP_VERSION}.tgz -P ${FILE_DIR}/"
+fi
+
+# 解压spark包
+if [[ ! -d ${FILE_DIR}/${SPARK_DIR} ]];then
+printf_and_run "tar -xvf ${SPARK_DIR}-bin-${HADOOP_VERSION}.tgz -C ${FILE_DIR}/${SPARK_DIR}/"
+printf_and_run "mv ${SPARK_DIR}-bin-${HADOOP_VERSION} ${SPARK_DIR}"
+fi
+
+#配置master地址
+cp ${SPARK_DIR}/conf/spark-env.sh.template ${SPARK_DIR}/conf/spark-env.sh
+export JAVA_HOME
+export SPARK_MASTER_HOST
+export SPARK_MASTER_PORT
+#配置slave地址
+cp ${SPARK_DIR}/conf/slaves.template ${SPARK_DIR}/conf/slaves
+echo "slave1" >>  ${SPARK_DIR}/conf/slaves
+
+
+# 复制spark包
+if [[ ! -d ${DOCKER_VOLUME_DIR}/${HADOOP_HOME_VOLUME} ]]; then
+printf_and_run "cp -r ${FILE_DIR}/${SPARK_DIR} ${DOCKER_VOLUME_DIR}/${SPARK_HOME_VOLUME}/_data/"
+fi
+
+# 复制配置到主数据卷
+#printf_and_run "\\cp -rf ${FILE_DIR}/build-support/conf/ ${DOCKER_VOLUME_DIR}/${SPARK_HOME_VOLUME}/_data/${SPARK_DIR}/"
+
+# 构建hadoop镜像
+printf_and_run "docker build --rm -t spark:v${SPARK_VERSION} ${FILE_DIR}/build-support"
+
+# 启动hadoop镜像，orphans：孤儿
+printf_and_run "docker-compose -p spark up --remove-orphans"
