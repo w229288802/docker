99d19299d24323a09cadd5c7dfdf72f98d65fe00
wuwj <229288802@qq.com>
2021-04-22 11:09:55 +0800

    update name

--- hadoop/hadoop-2.7.5/build-support/docker-entrypoint.sh
+++ hadoop/hadoop-2.7.5/build-support/docker-entrypoint.sh
@@ -8,6 +8,8 @@ println(){ printf "${BLACK_COLOR}$1 ${NO_COLOR}\n";}
 #配置变量
 HADOOP_HOME=/opt/hadoop-2.7.5
 HADOOP_DATA=/data/hadoop
+HADOOP_MASTER_HOSTNAME=hadoop-master
+HADOOP_SLAVE1_HOSTNAME=hadoop-slave1
 PATH=$PATH:${HADOOP_HOME}/bin
 
 #导出变量
@@ -15,36 +17,35 @@ echo "export HADOOP_HOME=${HADOOP_HOME}">> ~/.bashrc
 echo "export PATH=${PATH}">> ~/.bashrc
 
 # 启动sshd
-/usr/sbin/sshd
+println_and_run "/usr/sbin/sshd"
 # 拷贝sshd秘钥
 mkdir -p /root/.ssh/
 cp /etc/ssh/ssh_host_rsa_key ~/.ssh/id_rsa
 cp /etc/ssh/ssh_host_rsa_key.pub ~/.ssh/authorized_keys
-echo -n "master " > ~/.ssh/known_hosts &&  sed -r "s|root@\w+||g" /etc/ssh/ssh_host_rsa_key.pub >>  ~/.ssh/known_hosts
-echo -n "slave1 " >> ~/.ssh/known_hosts &&  sed -r "s|root@\w+||g" /etc/ssh/ssh_host_rsa_key.pub >> ~/.ssh/known_hosts
-
-#
+echo -n "${HADOOP_MASTER_HOSTNAME} " > ~/.ssh/known_hosts &&  sed -r "s|root@\w+||g" /etc/ssh/ssh_host_rsa_key.pub >>  ~/.ssh/known_hosts
+echo -n "${HADOOP_SLAVE1_HOSTNAME} " >> ~/.ssh/known_hosts &&  sed -r "s|root@\w+||g" /etc/ssh/ssh_host_rsa_key.pub >> ~/.ssh/known_hosts
+#格式化namenode
 if [[ ! -d ${HADOOP_DATA} ]];then
-mkdir ${HADOOP_DATA}
-hadoop namenode -format
+println_and_run "mkdir ${HADOOP_DATA}"
+println_and_run "hadoop namenode -format"
 fi
 
 # 开启hadoop
-if [[ "${HOSTNAME}" == "master" ]]; then
+if [[ "${HOSTNAME}" == "hadoop-master" ]]; then
 println_and_run "sed -i 's|\${JAVA_HOME}|${JAVA_HOME}|' ${HADOOP_HOME}/etc/hadoop/hadoop-env.sh"
 println_and_run "${HADOOP_HOME}/sbin/start-all.sh"
 # 注意！！！如果是slave, 需等待 master退出,不然会找不到slave，也可以用sleep等待
 else
-while nc -w 1 master 22 </dev/null >/dev/null 2>&1;[[ $? == 1 || $? == 2 ]] #等待master启动
+while nc -w 1 ${HADOOP_MASTER_HOSTNAME} 22 </dev/null >/dev/null 2>&1;[[ $? == 1 || $? == 2 ]] #等待master启动
 do
  sleep 0.2s
 done
-println "check activated master"
+println "check activated ${HADOOP_MASTER_HOSTNAME}"
 
-while nc -w 1 master 22 </dev/null >/dev/null 2>&1;[[ $? == 0 || $? == 2 ]] #等待master停止
+while nc -w 1 ${HADOOP_MASTER_HOSTNAME} 22 </dev/null >/dev/null 2>&1;[[ $? == 0 || $? == 2 ]] #等待master停止
 do
   sleep 0.2s
 done
-println "check inactivated master"
+println "check inactivated ${HADOOP_MASTER_HOSTNAME}"
 fi
 tail -f /dev/null
--- hadoop/hadoop-2.7.5/start-after-build.sh
+++ hadoop/hadoop-2.7.5/start-after-build.sh
@@ -7,7 +7,7 @@ println(){ printf "${BLACK_COLOR}$1 ${NO_COLOR}\n";}
 
 #配置变量
 DOCKER_VOLUME_DIR=/var/lib/docker/volumes
-HADOOP_HOME_VOLUME=hadoop_opt
+HADOOP_HOME_VOLUME=hadoop-opt
 HADOOP_VERSION=2.7.5
 HADOOP_DIR=hadoop-${HADOOP_VERSION}
 FILE_DIR=$(cd $(dirname $0); pwd)
@@ -16,6 +16,7 @@ ${FILE_DIR}/stop-after-build.sh
 
 # 下载hadoop包
 if [[ ! -f ${FILE_DIR}/${HADOOP_DIR}.tar.gz ]];then
+yum install -y wget
 println_and_run "wget http://archive.apache.org/dist/hadoop/core/${HADOOP_DIR}/${HADOOP_DIR}.tar.gz -P ${FILE_DIR}/"
 fi
 
@@ -26,14 +27,14 @@ fi
 
 # 创建hadoop主数据卷
 if [[ ! -d ${DOCKER_VOLUME_DIR}/${HADOOP_HOME_VOLUME} ]]; then
-println_and_run "docker volume create ${HADOOP_HOME_VOLUME}"
+println_and_run "docker volume create ${HADOOP_HOME_VOLUME} >/dev/null"
 println_and_run "cp -r ${FILE_DIR}/${HADOOP_DIR} ${DOCKER_VOLUME_DIR}/${HADOOP_HOME_VOLUME}/_data/"
 fi
 
 # 创建网络 docker network create hadoop --gateway=172.18.0.1 --subnet=172.18.0.0/1
 exist_hadoop_net=`docker network ls |grep -E "\s+hadoop\s+" |wc -l`
 if [[ ${exist_hadoop_net} -eq 0 ]];then
-docker network create hadoop --gateway=172.18.0.1 --subnet=172.18.0.0/1
+docker network create hadoop --gateway=172.18.0.1 --subnet=172.18.0.0/16
 fi
 
 # 复制配置到主数据卷
