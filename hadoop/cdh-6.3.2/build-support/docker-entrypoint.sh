bb27fe0ccb6213408098c3755a3fc5e177eef3b5
wuwj <229288802@qq.com>
2021-04-29 18:04:10 +0800

    cdh

---
+++ hadoop/cdh-6.3.2/build-support/docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+BLACK_COLOR='\033[0;35m'
+NO_COLOR='\033[0m'
+
+println_and_run() { printf "${BLACK_COLOR}$1 ${NO_COLOR}\n"; bash -c "$1";}
+println(){ printf "${BLACK_COLOR}$1 ${NO_COLOR}\n";}
+
+#配置变量
+APP_NAME=hive
+HIVE_DATA=/data/hadoop
+HIVE_VERSION=2.1.1
+HIVE_DIR=apache-${APP_NAME}-${HIVE_VERSION}-bin
+HIVE_HOME=/opt/${HIVE_DIR}
+CDH_MASTER_HOSTNAME=cdh6
+HIVE_SLAVE1_HOSTNAME=hive-slave1
+export PATH=$PATH:${HIVE_HOME}/bin
+
+#导出变量
+echo "export HIVE_HOME=${HIVE_HOME}">> ~/.bashrc
+echo "export PATH=${PATH}">> ~/.bashrc
+
+echo 111
+#/usr/sbin/init
+#systemctl restart autofs
+#/usr/bin/systemctl restart autofs &
+#/usr/sbin/init
+#systemctl restart autofs
+#yum install -y oracle-j2sdk1.8.x86_64
+#yum install -y cloudera-manager-daemons cloudera-manager-agent cloudera-manager-server cloudera-manager-daemons cloudera-manager-agent
+#systemctl start autofs
+#sed -i "s/server_host=localhost/server_host=${CDH_MASTER_HOSTNAME}/g" /etc/cloudera-scm-agent/config.ini
+tail -f /dev/null
---
+++ hadoop/cdh-6.3.2/start-after-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+BLACK_COLOR='\033[0;35m'
+NO_COLOR='\033[0m'
+
+println_and_run() { printf "${BLACK_COLOR}$1 ${NO_COLOR}\n"; bash -c "$1";}
+println(){ printf "${BLACK_COLOR}$1 ${NO_COLOR}\n";}
+
+#配置变量
+DOCKER_VOLUME_DIR=/var/lib/docker/volumes
+FILE_PATH=$(cd $(dirname $0); pwd)
+export APP_NAME=cdh6
+
+${FILE_PATH}/stop-after-build.sh
+
+# 启动hadoop镜像，orphans：孤儿
+println_and_run "docker-compose -f ${FILE_PATH}/docker-compose.yml -p ${APP_NAME} up --remove-orphans --build"
