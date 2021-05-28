e4a879d3ba109196d975d8b60cbc0544ae456b79
wuwj <229288802@qq.com>
2021-05-28 09:26:43 +0800

    hue

--- hadoop/hue-v3.9.0-cdh5.14.0/build-support/docker-entrypoint.sh
+++ hadoop/hue-v3.9.0-cdh5.14.0/build-support/docker-entrypoint.sh
@@ -14,9 +14,16 @@ PATH=$PATH:${HADOOP_HOME}/bin
 APP_NAME=impala
 
 #导出变量
+#fs_defaultfs=hdfs://hadoop-master:9000
+#webhdfs_url=http://hadoop-master:50070/webhdfs/v1
+sed -i "s|fs_defaultfs=hdfs://localhost:8020|fs_defaultfs=hdfs://hadoop-master:9000|g" /etc/hue/conf/hue.ini
 
-sed -i "s/## hive_server_host=localhost/hive_server_host=hive-master/g" /etc/hue/conf/hue.ini
+sed -i "s|## webhdfs_url=http://localhost:50070/webhdfs/v1|webhdfs_url=http://hadoop-master:50070/webhdfs/v1|g" /etc/hue/conf/hue.ini
+
+sed -i "s/## hive_server_host=localhost/hive_server_host=hive/g" /etc/hue/conf/hue.ini
 
 sed -i "s/## server_host=localhost/server_host=impala-master/g" /etc/hue/conf/hue.ini
+
+service hue start
 
 tail -f /dev/null
